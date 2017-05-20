a1a8cbe82e94b28b1272e630f24546d99e783b9d
Yusent Chig <yusent@protonmail.com>
2017-05-20 14:34:42 -0700

    Add build script

---
+++ build.js
@@ -0,0 +1,25 @@
+let fs = require('fs');
+let mkdirp = require('mkdirp');
+let sass = require('node-sass');
+
+sass.render({
+  file: 'sass/matter.sass',
+}, function (renderError, result) {
+  if (renderError) {
+    console.log(renderError);
+  } else {
+    mkdirp('dist/css', function (mkdirError) {
+      if (mkdirError) {
+        console.log(mkdirError);
+      } else {
+        fs.writeFile('dist/css/matter.css', result.css, function (writeError) {
+          if (writeError) {
+            console.log(writeError);
+          } else {
+            console.log('dist/css/matter.css generated!');
+          }
+        });
+      }
+    });
+  }
+});
