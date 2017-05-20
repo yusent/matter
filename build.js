9acb0f7396da889dab0182cd22ba2d7f91883c82
Yusent Chig <yusent@protonmail.com>
2017-05-20 16:17:54 -0700

    Use expanded style in rendered css

--- build.js
+++ build.js
@@ -4,6 +4,8 @@ let sass = require('node-sass');
 
 sass.render({
   file: 'sass/matter.sass',
+  indentedSyntax: true,
+  outputStyle: 'expanded',
 }, function (renderError, result) {
   if (renderError) {
     console.log(renderError);
