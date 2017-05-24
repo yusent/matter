d939e827de63118c3cca5b615972a071c70c4af7
Yusent Chig <yusent@protonmail.com>
2017-05-24 08:32:05 -0700

    Test grid system against multiple breakpoints

--- karma.conf.js
+++ karma.conf.js
@@ -9,15 +9,80 @@ module.exports = function (config) {
         }],
       ],
     },
-    browsers: ['PhantomJS_Desktop'],
+    client: {
+      args: ['--grep', config.grep],
+    },
     customLaunchers: {
-      'PhantomJS_Desktop': {
+      'PhantomJS_XXS': {
+        base: 'PhantomJS',
+        options: {
+          viewportSize: {
+            width: 479,
+            height: 1000,
+          },
+        },
+      },
+      'PhantomJS_XS': {
+        base: 'PhantomJS',
+        options: {
+          viewportSize: {
+            width: 480,
+            height: 1000,
+          },
+        },
+      },
+      'PhantomJS_S': {
+        base: 'PhantomJS',
+        options: {
+          viewportSize: {
+            width: 600,
+            height: 1000,
+          },
+        },
+      },
+      'PhantomJS_M': {
         base: 'PhantomJS',
         options: {
           viewportSize: {
             width: 840,
             height: 1000,
-          }
+          },
+        },
+      },
+      'PhantomJS_L': {
+        base: 'PhantomJS',
+        options: {
+          viewportSize: {
+            width: 960,
+            height: 1000,
+          },
+        },
+      },
+      'PhantomJS_XL': {
+        base: 'PhantomJS',
+        options: {
+          viewportSize: {
+            width: 1280,
+            height: 1000,
+          },
+        },
+      },
+      'PhantomJS_XXL': {
+        base: 'PhantomJS',
+        options: {
+          viewportSize: {
+            width: 1440,
+            height: 1000,
+          },
+        },
+      },
+      'PhantomJS_XXXL': {
+        base: 'PhantomJS',
+        options: {
+          viewportSize: {
+            width: 1600,
+            height: 1000,
+          },
         },
       },
     },
--- test_with_browser/grid.js
+++ test_with_browser/grid.js
@@ -1,32 +1,59 @@
-let assert = require('assert');
+import assert from 'assert';
+
+let displaySizes = ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl'];
+
 let rows = [
   ['xxs1', 'xxs3'],
+  ['xs2', 'xs2'],
   ['s2', 's6'],
   ['m10', 'm2'],
+  ['l6', 'l6'],
+  ['xl10', 'xl2'],
+  ['xxl4', 'xxl2', 'xxl6'],
+  ['xxxl11', 'xxxl1'],
 ];
 
-describe('grid system', function () {
-  describe('mt-column', function () {
-    it('displays side by side with siblings when they fit', function () {
-      rows.forEach(function (sizes) {
-        let row = document.createElement('div');
-        row.className = 'mt-row';
+function fits(displaySize, colSize) {
+  let _colSize = colSize.replace(/^mt-([a-z]+)\d+$/, '$1');
+  return displaySizes.indexOf(_colSize) <= displaySizes.indexOf(displaySize);
+}
 
-        sizes.forEach(function (size) {
-          let col = document.createElement('div');
-          col.className = `mt-col mt-${size}`;
-          col.appendChild(document.createTextNode(size));
-          row.appendChild(col);
-        });
+for (let size of displaySizes) {
+  describe(`grid system when display size is ${size}`, function () {
+    describe('mt-column', function () {
+      it('displays side by side with siblings when they fit', function () {
+        for (let columnSizes of rows) {
+          let row = document.createElement('div');
+          row.className = 'mt-row';
 
-        document.body.appendChild(row);
-        let rowRect = row.getBoundingClientRect();
+          for (let columnSize of columnSizes) {
+            let col = document.createElement('div');
+            col.className = `mt-col mt-${columnSize}`;
+            col.appendChild(document.createTextNode(columnSize));
+            row.appendChild(col);
+          }
 
-        for (let col of row.childNodes) {
-          let colRect = col.getBoundingClientRect();
-          assert.equal(colRect.top, rowRect.top);
+          document.body.appendChild(row);
+          let rowRect = row.getBoundingClientRect();
+
+          for (let col of row.childNodes) {
+            let colRect = col.getBoundingClientRect();
+            let itFits = col.className
+              .replace('mt-col ', '')
+              .split(/\s/)
+              .some(c => fits(size, c));
+
+            if (itFits) {
+              assert.equal(colRect.top, rowRect.top);
+            } else {
+              let style = getComputedStyle(col);
+              let margin = parseFloat(style.marginLeft);
+
+              assert.equal(row.clientWidth, col.offsetWidth + margin * 2);
+            }
+          }
         }
       });
     });
   });
-});
+}
