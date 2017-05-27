f71b49d8db88ece2125a3ad080a4494942f0f6e8
Yusent Chig <yusent@protonmail.com>
2017-05-27 15:27:35 -0700

    Add ripple animation

--- dist/js/matter.js
+++ dist/js/matter.js
@@ -7,17 +7,31 @@ Object.defineProperty(exports, "__esModule", {
 
 exports.default = function (button) {
   (0, _functions.addHandler)(button, 'mouseleave', function () {
-    return removeRippleFrom(button);
+    return unpress(button);
   });
 
   (0, _functions.addHandler)(button, 'mouseup', function () {
-    return removeRippleFrom(button);
+    return unpress(button);
   });
 
   (0, _functions.addHandler)(button, 'mousedown', function (event) {
     if (event.button !== 2) {
+      button.classList.add('pressed');
+
       var ripple = document.createElement('div');
-      ripple.className = 'ripple';
+      ripple.className = 'ripple animating';
+      ripple.style.height = button.offsetWidth * 2 + 'px';
+      ripple.style.top = event.pageY - this.offsetTop - button.offsetWidth + 'px';
+      ripple.style.left = event.pageX - this.offsetLeft - button.offsetWidth + 'px';
+
+      (0, _functions.addHandler)(ripple, 'animationend', function () {
+        ripple.classList.remove('animating');
+
+        if (!button.classList.contains('pressed')) {
+          button.removeChild(ripple);
+        }
+      });
+
       button.appendChild(ripple);
     }
   });
@@ -25,10 +39,11 @@ exports.default = function (button) {
 
 var _functions = require('./functions');
 
-function removeRippleFrom(button) {
+function unpress(button) {
+  button.classList.remove('pressed');
   var ripple = button.querySelector('.ripple');
 
-  if (ripple) {
+  if (ripple && !ripple.classList.contains('animating')) {
     button.removeChild(ripple);
   }
 }
--- js/buttons.js
+++ js/buttons.js
@@ -1,22 +1,37 @@
 import { addHandler } from './functions';
 
-function removeRippleFrom(button) {
+function unpress(button) {
+  button.classList.remove('pressed');
   let ripple = button.querySelector('.ripple');
 
-  if (ripple) {
+  if (ripple && !ripple.classList.contains('animating')) {
     button.removeChild(ripple);
   }
 }
 
 export default function (button) {
-  addHandler(button, 'mouseleave', () => removeRippleFrom(button));
+  addHandler(button, 'mouseleave', () => unpress(button));
 
-  addHandler(button, 'mouseup', () => removeRippleFrom(button));
+  addHandler(button, 'mouseup', () => unpress(button));
 
   addHandler(button, 'mousedown', function (event) {
     if (event.button !== 2) {
+      button.classList.add('pressed');
+
       let ripple = document.createElement('div');
-      ripple.className = 'ripple';
+      ripple.className = 'ripple animating';
+      ripple.style.height = `${button.offsetWidth * 2}px`;
+      ripple.style.top = `${event.pageY - this.offsetTop - button.offsetWidth}px`;
+      ripple.style.left = `${event.pageX - this.offsetLeft - button.offsetWidth}px`;
+
+      addHandler(ripple, 'animationend', function () {
+        ripple.classList.remove('animating');
+
+        if (!button.classList.contains('pressed')) {
+          button.removeChild(ripple);
+        }
+      });
+
       button.appendChild(ripple);
     }
   });
